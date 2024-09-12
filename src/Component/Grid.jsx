b173ecf4e783dd71485ea6100a018ee6c483ff63
VISHALSHARMAVS <vishalsharmakt143@gamil.com>
2024-09-12 23:33:40 +0530

    error bugs

--- src/Component/Grid.jsx
+++ src/Component/Grid.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import  { useMemo } from 'react';
-import './grid.css';
+import './Grid.css';
 import Column from './Column';
 
 function Grid({ gridData, grouping, userIdToData }) {
